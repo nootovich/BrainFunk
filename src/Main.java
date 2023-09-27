c47e78ce4377da569928ac27df66e5c3cefeb3a9
nootovich <mailnonamer@gmail.com>
2023-09-27 18:46:35 +0300

    Added 'patterns' (something like inline function)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 
 public class Main {
 
     static boolean DEBUG = false;
 
-    static byte[] tape    = new byte[256];
-    static int    pointer = 0;
+    static byte[]                  tape     = new byte[256];
+    static int                     pointer  = 0;
+    static int                     i        = 0;
+    static HashMap<String, String> patterns = new HashMap<>();
 
     public static void main(String[] args) {
         if (args.length == 0) exit("No argument were provided!");
@@ -17,13 +20,24 @@ public class Main {
     }
 
     public static void executeChunk(char[] inputData) {
-        int repetitionCount = 0;
-        for (int i = 0; i < inputData.length; i++) {
+        int           repetitionCount = 0;
+        boolean       nameStarted     = false;
+        StringBuilder name            = new StringBuilder();
+        for (; i < inputData.length; i++) {
             char c = inputData[i];
-            if (Character.isDigit(c)) {
-                repetitionCount = repetitionCount*10+c-'0';
+            if (!nameStarted) {
+                if (Character.isDigit(c)) {
+                    repetitionCount = repetitionCount*10+c-'0';
+                    continue;
+                } else if (Character.isLetter(c)) {
+                    nameStarted = true;
+                    name.append(c);
+                    continue;
+                }
+            } else if (Character.isLetterOrDigit(c)) {
+                name.append(c);
                 continue;
-            }
+            } else if (c != ':') exit("Unexpected character '"+c+"'\nFrom: "+i);
             switch (c) {
                 case '+' -> tape[pointer] += (byte) (repetitionCount > 0 ? repetitionCount : 1);
                 case '-' -> tape[pointer] -= (byte) (repetitionCount > 0 ? repetitionCount : 1);
@@ -47,15 +61,18 @@ public class Main {
                 }
                 case ']' -> {}
                 case ',' -> System.out.println("\nInput is not implemented yet\n");
-                case '@' -> syscall(inputData, i);
+                case '@' -> syscall(inputData);
+                case ':' -> addPattern(inputData, name.toString());
                 default -> exit("Unknown character '"+c+"'");
             }
             pointer         = pointer%tape.length;
             repetitionCount = 0;
+            nameStarted     = false;
+            name.setLength(0);
         }
     }
 
-    public static void syscall(char[] data, int i) {
+    public static void syscall(char[] data) {
         if (i >= data.length-1) exit("Amount of arguments for syscall was not provided");
         switch (data[i+1]) {
             case '1' -> syscall1();
@@ -68,14 +85,26 @@ public class Main {
         System.exit(tape[pointer-1]);
     }
 
+    public static void addPattern(char[] input, String name) {
+        int           start   = i;
+        StringBuilder pattern = new StringBuilder();
+        for (i++; i < input.length; i++) {
+            char c = input[i];
+            if (c == ';') break;
+            pattern.append(c);
+        }
+        if (input[i] != ';') exit("Unmatched semicolon!\nFrom: "+start);
+        patterns.put(name, pattern.toString());
+    }
+
     public static char[] preprocessData(char[] inputData) {
-        char[]        allowed   = new char[]{'+', '-', '>', '<', '[', ']', '.', ',', '@'};
+        char[]        allowed   = new char[]{'+', '-', '>', '<', '[', ']', '.', ',', '@', ':', ';'};
         StringBuilder processed = new StringBuilder();
         for (int i = 0; i < inputData.length; i++) {
             char c = inputData[i];
             if (c == '/' && i+1 < inputData.length && inputData[i+1] == '/') {
                 while (i < inputData.length && inputData[i] != '\n') i++;
-            } else if (Character.isDigit(c)) {
+            } else if (Character.isLetterOrDigit(c)) {
                 processed.append(c);
             } else for (char value: allowed)
                 if (c == value) {
---
+++ src/patterns.bf
@@ -0,0 +1,6 @@
+add10:10+;
+dup:[->+>+<<]>>[-<<+>>]<<;
+print:.;
+
+// "hi!" - 104 105 33
+add10 add10 add10 add10 add10 add10 add10 add10 add10 add10 4+ dup print > + print > add10 add10 add10 3+ print
