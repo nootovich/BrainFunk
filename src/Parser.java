e66ccd2a0da2f6054d5749149ba0181a294335aa
nootovich <mailnonamer@gmail.com>
2023-11-25 14:29:18 +0300

    Fixed a bug where the same number could be used by a pointer and a macro at the same time, thus leaving a pointer without a value to go to

--- src/Parser.java
+++ src/Parser.java
@@ -30,7 +30,8 @@ public class Parser {
                 macros.put(tk.strValue, tk.macroTokens);
             } else if (tk.type == Token.Type.MACRO) {
                 int amount = 1;
-                if (!parsed.isEmpty() && parsed.peek().type == Token.Type.NUMBER) amount = parsed.pop().numValue;
+                if (parsed.size() > 1 && parsed.peek().type == Token.Type.NUMBER && parsed.get(parsed.size()-2).type != Token.Type.POINTER)
+                    amount = parsed.pop().numValue;
                 if (!macros.containsKey(tk.strValue)) error("Undefined macro %s.".formatted(tk));
                 List<Token> macroTokens = List.of(parseMacros(macros.get(tk.strValue), tk));
                 for (int j = 0; j < amount; j++) parsed.addAll(macroTokens);
