27079ab2e871917c675849ea054be624206b14d3
nootovich <mailnonamer@gmail.com>
2024-02-19 22:34:12 +0300

    Fixed how column values of tokens inside macros were assigned. This was causing issues in `Debugger`

--- src/Lexer.java
+++ src/Lexer.java
@@ -11,17 +11,19 @@ public class Lexer {
         showTokens = Main.showTokens;
         String rawData = FileSystem.loadFile(filepath);
         if (rawData == null) error("Could not get file data.");
-        return lexData(rawData, 0).toArray(new Token[0]);
+        return lexData(rawData, 0, 0).toArray(new Token[0]);
     }
 
-    protected static ArrayList<Token> lexData(String data, int rowOffset) {
+    protected static ArrayList<Token> lexData(String data, int rowOffset, int colOffset) {
         String[]         lines  = data.split("\n");
         ArrayList<Token> tokens = new ArrayList<>();
         for (int dataRow = 0; dataRow < lines.length; dataRow++) {
             int    row  = dataRow + rowOffset;
             String line = lines[dataRow];
-            for (int col = 0; col < line.length(); col++) {
-                char c = line.charAt(col);
+
+            for (int dataCol = 0; dataCol < line.length(); dataCol++) {
+                int col = dataCol + colOffset;
+                char c = line.charAt(dataCol);
 
                 Token.Type tokenType = null;
                 Token      tk        = null;
@@ -38,54 +40,49 @@ public class Lexer {
                 if (tokenType != null) {tk = new Token(tokenType, filename, row, col);}
 
                 // BFN STUFF
-                else if (c == '/' && col < line.length() - 1 && line.charAt(col + 1) == '/') break;
+                else if (c == '/' && dataCol < line.length() - 1 && line.charAt(dataCol + 1) == '/') break;
                 else if (c == '$') tk = new Token(Token.Type.POINTER, filename, row, col);
-                else if (c == '#') tk = new Token(Token.Type.RETURN, filename, row, col);
+                else if (c == '#') tk = new Token(Token.Type.RETURN, filename, row,  col);
                 else if (c == ':') error("Unfinished macro definition at " + new Token(Token.Type.ERROR, filename, row, col));
                 else if (c == '"') {
-                    int start = col;
-                    while (col < line.length() - 1 && line.charAt(++col) != '"') {}
-                    tk = new Token(Token.Type.STRING, filename, row, start);
-                    if (line.charAt(col) != '"' || col - start < 1) error("Unfinished string literal at " + tk);
-                    tk.strValue = line.substring(start + 1, col);
+                    int start = dataCol;
+                    while (dataCol < line.length() - 1 && line.charAt(++dataCol) != '"') {}
+                    tk = new Token(Token.Type.STRING, filename, row, start + colOffset);
+                    if (line.charAt(dataCol) != '"' || dataCol - start < 1) error("Unfinished string literal at " + tk);
+                    tk.strValue = line.substring(start + 1, dataCol);
                 } else if (Character.isDigit(c)) {
-                    int start = col;
+                    int start = dataCol;
                     int val   = c - '0';
-                    while (col < line.length() - 1 && Character.isDigit(line.charAt(col + 1))) val = val * 10 + line.charAt(++col) - '0';
-                    tk = new Token(Token.Type.NUMBER, filename, row, start);
+                    while (dataCol < line.length() - 1 && Character.isDigit(line.charAt(dataCol + 1))) val = val * 10 + line.charAt(++dataCol) - '0';
+                    tk = new Token(Token.Type.NUMBER, filename, row, start + colOffset);
                     if (val < 0) error("Invalid value for a `NUMBER` token `" + val + "` at " + tk);
                     tk.numValue = val;
                 } else if (Character.isLetter(c)) {
-                    int startCol = col;
-                    for (; col < line.length(); col++) if (!Character.isLetterOrDigit(line.charAt(col))) break;
-                    if (col - startCol < 0) error("Unfinished macro definition at " + new Token(Token.Type.ERROR, filename, row, col));
-                    if (col < line.length() && line.charAt(col) == ':') {
-                        tk          = new Token(Token.Type.MACRODEF, filename, row, startCol);
-                        tk.strValue = line.substring(startCol, col);
-                        startCol    = ++col;
+                    int startCol = dataCol;
+                    for (; dataCol < line.length(); dataCol++) if (!Character.isLetterOrDigit(line.charAt(dataCol))) break;
+                    if (dataCol - startCol < 0) error("Unfinished macro definition at " + new Token(Token.Type.ERROR, filename, row, col));
+                    if (dataCol < line.length() && line.charAt(dataCol) == ':') {
+                        tk          = new Token(Token.Type.MACRODEF, filename, row, startCol + colOffset);
+                        tk.strValue = line.substring(startCol, dataCol);
+                        startCol    = ++dataCol;
                         showTokens     = false;
                         ArrayList<Token> macroTokens     = new ArrayList<>();
                         for (int col2 = startCol; dataRow < lines.length; dataRow++) {
                             line = lines[dataRow];
-                            for (; col < line.length(); col++) {
-                                if (line.charAt(col) == ';') break;
+                            for (; dataCol < line.length(); dataCol++) {
+                                if (line.charAt(dataCol) == ';') break;
                             }
-                            macroTokens.addAll(lexData(line.substring(col2, col), row));
-                            if (col < line.length() && line.charAt(col) == ';') break;
-                            col = 0;
-                            col2 = 0;
-                        }
-                        if (col == line.length() || line.charAt(col) != ';') {
-                            tk.col = col;
-                            error("Unfinished macro body at " + tk);
+                            macroTokens.addAll(lexData(line.substring(col2, dataCol), dataRow, col2 + colOffset));
+                            if (dataCol < line.length() && line.charAt(dataCol) == ';') break;
+                            dataCol = 0;
+                            col2    = 0;
                         }
                         tk.macroTokens = macroTokens.toArray(new Token[0]);
-                        for (Token t: tk.macroTokens) t.col += startCol;
                         showTokens = Main.showTokens;
                     } else {
-                        tk          = new Token(Token.Type.MACRO, filename, row, startCol);
-                        tk.strValue = line.substring(startCol, col);
-                        col--;
+                        tk          = new Token(Token.Type.MACRO, filename, row, startCol + colOffset);
+                        tk.strValue = line.substring(startCol, dataCol);
+                        dataCol--;
                     }
                 }
                 if (tk != null) tokens.add(tk);
