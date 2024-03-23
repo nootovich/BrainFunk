b094557e6cadda5ddfe83d6b6f3860d3cdff6b26
nootovich <mailnonamer@gmail.com>
2024-03-23 17:52:52 +0300

    Refactored `Lexer`

--- src/Token.java
+++ src/Token.java
@@ -1,10 +1,10 @@
 public class Token {
 
     public enum Type {
-        ADD, SUB, PTRADD, PTRSUB, WHILE, ENDWHILE, WRITE, READ, // VANILLA
-        NUMBER, STRING, MACRODEF, MACRO, POINTER, RETURN,
-        COLON, UNSAFEWHILE, UNSAFEENDWHILE,
-        ERROR
+        INC, DEC, RGT, LFT, INP, OUT, JEZ, JNZ, // VANILLA
+        NUM, STR, PTR, RET, WRD,
+        COL, SCL, UNSAFEJEZ, UNSAFEJNZ,
+        ERR
     }
 
     public Type    type;
@@ -30,24 +30,25 @@ public class Token {
 
     public String repr() {
         return switch (type) {
-            case ADD -> "+";
-            case SUB -> "-";
-            case PTRADD -> ">";
-            case PTRSUB -> "<";
-            case WHILE -> "[";
-            case ENDWHILE -> "]";
-            case WRITE -> ".";
-            case READ -> ",";
-            case NUMBER -> String.valueOf(numValue);
-            case STRING -> '\"' + strValue + '\"';
-            case MACRODEF -> strValue + ':';
-            case MACRO -> strValue;
-            case POINTER -> "$";
-            case RETURN -> "#";
-            case COLON -> ":";
-            case UNSAFEWHILE -> "{";
-            case UNSAFEENDWHILE -> "}";
-            case ERROR -> throw new RuntimeException("Attempted to get a representation of an `ERROR` token... HOW!?");
+            case INC -> "+";
+            case DEC -> "-";
+            case RGT -> ">";
+            case LFT -> "<";
+            case JEZ -> "[";
+            case JNZ -> "]";
+            case OUT -> ".";
+            case INP -> ",";
+            case NUM -> String.valueOf(numValue);
+            case STR -> '\"' + strValue + '\"';
+            // case MACRODEF -> strValue + ':';
+            case WRD -> strValue;
+            case PTR -> "$";
+            case RET -> "#";
+            case COL -> ":";
+            case SCL -> ";";
+            case UNSAFEJEZ -> "{";
+            case UNSAFEJNZ -> "}";
+            case ERR -> throw new RuntimeException("Attempted to get a representation of an `ERROR` token... HOW!?");
         };
     }
 
@@ -72,13 +73,14 @@ public class Token {
     public String toString() {
         StringBuilder result = new StringBuilder("%s:%d:%d [%s".formatted(file, row + 1, col, type));
 
-        if (type == Type.NUMBER || type == Type.POINTER) result.append("(").append(numValue).append(")]");
-        else if (type == Type.STRING || type == Type.MACRO) result.append("(").append(strValue).append(")]");
-        else if (type == Type.MACRODEF) {
-            result.append("(").append(strValue).append(")] {");
-            if (macroTokens != null) for (Token macroToken: macroTokens) result.append("%n    %s".formatted(macroToken));
-            result.append("\n}");
-        } else result.append("]");
+        if (type == Type.NUM || type == Type.PTR) result.append("(").append(numValue).append(")]");
+        else if (type == Type.STR || type == Type.WRD) result.append("(").append(strValue).append(")]");
+        // else if (type == Type.MACRODEF) {
+        //     result.append("(").append(strValue).append(")] {");
+        //     if (macroTokens != null) for (Token macroToken: macroTokens) result.append("%n    %s".formatted(macroToken));
+        //     result.append("\n}");
+        // }
+        else result.append("]");
         if (origin != null) result.append("\n    [INFO]: Expanded from: ").append(origin);
         return result.toString();
     }
