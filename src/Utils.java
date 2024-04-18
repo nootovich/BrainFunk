3a284c585cc1fe32d62b79cc06af4fd94ebc15a1
nootovich <mailnonamer@gmail.com>
2024-04-18 22:09:11 +0300

    Slight visual improvements of info/error messages

--- src/Utils.java
+++ src/Utils.java
@@ -1,5 +1,7 @@
 public class Utils {
 
+    private static final int FILENAME_LEN = 20;
+
     public static int clampi(int n, int min, int max) {
         if (max < min) max = min;
         if (n < min) return min;
@@ -8,13 +10,19 @@ public class Utils {
     }
 
     public static void info(String message) {
-        StackTraceElement src = Thread.currentThread().getStackTrace()[2];
-        System.out.printf("%s:%d [INFO]: %s%n", src.getFileName(), src.getLineNumber(), message);
+        StackTraceElement src      = Thread.currentThread().getStackTrace()[2];
+        String            filename = src.getFileName();
+        String            lineNum  = String.valueOf(src.getLineNumber());
+        String            padding  = " ".repeat(FILENAME_LEN - filename.length() - lineNum.length());
+        System.out.printf("%s:%s%s [INFO]:  %s%n", filename, lineNum, padding, message);
     }
 
     public static void error(String message) {
-        StackTraceElement errSrc = Thread.currentThread().getStackTrace()[2];
-        System.out.printf("%s:%d [ERROR]: %s%n", errSrc.getFileName(), errSrc.getLineNumber(), message);
+        StackTraceElement src      = Thread.currentThread().getStackTrace()[2];
+        String            filename = src.getFileName();
+        String            lineNum  = String.valueOf(src.getLineNumber());
+        String            padding  = " ".repeat(FILENAME_LEN - filename.length() - lineNum.length());
+        System.out.printf("%s:%s%s %s[ERROR]%s: %s%n", filename, lineNum, padding, "\u001B[31m", "\u001B[0m", message);
         System.exit(1);
     }
 }
