9b2acfcc5e280494a79d5bcbf6f1a755cbf96ffa
nootovich <mailnonamer@gmail.com>
2024-04-20 22:08:01 +0300

    `Main.java` bugfix IDEA keeps breaking my commits, thanks a lot...

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        boolean profiling = false;
         boolean profiling          = false;
         boolean transpile          = false;
         String  transpiledFilepath = null;
