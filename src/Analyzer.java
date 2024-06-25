4b518afc6c795e1c8971fcf9bdd44c7829095140
nootovich <mailnonamer@gmail.com>
2024-06-25 17:20:16 +0300

    Updated look a bit

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -8,10 +8,10 @@ public class Analyzer {
     public static void main(String[] args) {
         if (args.length < 1)
             Utils.error("No file was provided. Please provide a `.bf`, `.bfn` or `.bfnx` file as a command line argument.");
-        String filepath = args[0];
-        String filename = new File(filepath).getName();
+        String   filepath      = args[0];
+        String   filename      = new File(filepath).getName();
         String[] filenameParts = filename.split("\\.");
-        String extension = filenameParts[filenameParts.length - 1];
+        String   extension     = filenameParts[filenameParts.length - 1];
         Main.ProgramType programType = switch (extension) {
             case "bf" -> Main.ProgramType.BF;
             case "bfn" -> Main.ProgramType.BFN;
@@ -24,7 +24,7 @@ public class Analyzer {
 
         String code = FileSystem.loadFile(filepath);
 
-        Token[] lexed = Lexer.lex(code, filepath, programType);
+        Token[] lexed  = Lexer.lex(code, filepath, programType);
         Token[] parsed = Parser.parse(Token.deepCopy(lexed), filepath);
         Interpreter.loadProgram(parsed, programType);
 
@@ -39,9 +39,9 @@ public class Analyzer {
         private final int w, h;
         private int cachedFontH, cachedFontW;
 
-        private final Graphics2D g2d;
+        private final Graphics2D    g2d;
         private final BufferedImage buffer;
-        private final Font font = new Font("Monospaced", Font.PLAIN, 16);
+        private final Font          font = new Font("Monospaced", Font.PLAIN, 16);
 
         public enum colorEnum {
             COLOR_BG, COLOR_DATA, COLOR_TEXT, COLOR_TEXT_FADED, COLOR_HIGHLIGHT, COLOR_SELECT, COLOR_CONNECTION
@@ -55,7 +55,7 @@ public class Analyzer {
                 new Color(0x5EF38C),
                 new Color(0xFFC69B),
                 new Color(0xD9E985),
-        };
+                };
 
         Window(int width, int height) {
 
@@ -89,17 +89,25 @@ public class Analyzer {
             g2d.setColor(new Color(colors[colorEnum.COLOR_BG.ordinal()].getRed(), colors[colorEnum.COLOR_BG.ordinal()].getGreen(), colors[colorEnum.COLOR_BG.ordinal()].getBlue(), 5));
             g2d.fillRect(0, 0, w, h);
 
-            g2d.setColor(Color.WHITE);
-            if (!Interpreter.finished && Interpreter.ip < Interpreter.tokens.length) {
-                int px = (Interpreter.ip * cachedFontW);
-                int x = px % w;
-                int y = (int) (h * 0.75f - Interpreter.pointer * 5);
-                g2d.drawRect(x, y, 1, 1);
-                g2d.drawRect(x + 2, (255 - ((int) Interpreter.tape[Interpreter.pointer] & 0xFF)) * 2 + 10, 1, 1);
-                g2d.drawString(Interpreter.tokens[Interpreter.ip].repr(), x, h - px / w * cachedFontH - 20 );
-                Interpreter.execute();
-            } else if (Interpreter.finished) {
-                Interpreter.restart();
+            for (int i = 0; i < 1000; i++) {
+                g2d.setColor(Color.WHITE);
+                if (!Interpreter.finished && Interpreter.ip < Interpreter.tokens.length) {
+                    int px = (Interpreter.ip * cachedFontW);
+                    int x  = px % w;
+                    int y  = (int) (h * 0.55f - Interpreter.pointer * 3);
+                    g2d.drawRect(x, y, 1, 1);
+//                    g2d.drawRect(x + 3, (256 - ((int) Interpreter.tape[Interpreter.pointer] & 0xFF)), 1, 1);
+                    g2d.drawRect(Interpreter.pointer * cachedFontW + 3, (256 - ((int) Interpreter.tape[Interpreter.pointer] & 0xFF)), 1, 1);
+                    g2d.drawString(Interpreter.tokens[Interpreter.ip].repr(), x, h - px / w * cachedFontH - 2);
+                    Interpreter.execute();
+                } else if (Interpreter.finished) {
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Interpreter.restart();
+                }
             }
 
             Insets insets = getInsets();
